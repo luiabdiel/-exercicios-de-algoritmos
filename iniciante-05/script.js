bb92ad695228d488ef9a59f050ae4bc28b56f65f
Luiggi Abdiel <102000310+luiabdiel@users.noreply.github.com>
2022-06-21 19:33:38 -0300

    Add files via upload

---
+++ iniciante-05/script.js
@@ -0,0 +1,6 @@
+const a = Number(prompt('Digite o primeiro valor: '))
+const b = Number(prompt('Digite o segundo valor: '))
+
+const resultado = Math.pow(a - b, 2)
+
+alert(`O resultado do quadrado da diferença entre A e B é: ${resultado}`)
